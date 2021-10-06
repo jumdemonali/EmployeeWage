e2a27b89599a685feebaa0079298f33e4c3bfdb0
jumdemonali <jumdemonali@gmail.com>
2021-10-06 20:28:13 +0530

    Added: Code to calculate monthly wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,26 +1,28 @@
-const IS_ABSENT=0;
-const IS_PART_TIME=1;
-const IS_FULL_TIME=2;
-const PART_TIME_HRS=4;
-const FULL_TIME_HRS=8;
-const EMP_WAGE_PER_HRS=20
+// Constants
+let EMP_RATE_PER_HOUR = 20;
+let IS_PART_TIME = 1;
+let IS_FULL_TIME = 2;
+let NUM_OF_WORKING_DAYS = 20;
 
+// Variables
+let isPresent = 1;
+let monthlyWage = 0;
+let empHrs = 0, empWage = 0;
 
-function getWorkingHours(empCheck){
-switch(empCheck) {
-    
+for(let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    switch(empCheck) {
     case IS_PART_TIME:
-        return PART_TIME_HRS;
-
+        empHrs = 4;
+        break
     case IS_FULL_TIME:
-        return FULL_TIME_HRS;
+        empHrs = 8;
+        break
     default:
-        return 0;
-   }    
+        empHrs = 0;
+    }
+    empWage = empHrs * EMP_RATE_PER_HOUR;
+    monthlyWage += empWage;
 }
 
-let empHrs=0;
-let empCheck=Math.floor(Math.random()*10)%3;
-empHrs = getWorkingHours(empCheck);
-let empWage=empHrs* EMP_WAGE_PER_HRS;
-console.log("Employee Wage is :" + empWage);
+console.log("Employee Monthly Wage = " + monthlyWage);
