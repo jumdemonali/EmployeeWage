aba345c1dc5fdb0970b6d9dcef53a4e38c13912d
jumdemonali <jumdemonali@gmail.com>
2021-10-06 20:43:41 +0530

    Added: Code to calculate monthly wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,28 +1,28 @@
-// Constants
-let EMP_RATE_PER_HOUR = 20;
-let IS_PART_TIME = 1;
-let IS_FULL_TIME = 2;
-let NUM_OF_WORKING_DAYS = 20;
+const IS_ABSENT=0;
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HRS=4;
+const FULL_TIME_HRS=8;
+const EMP_WAGE_PER_HRS=20;
+const NUM_OF_WORKING_DAYS= 20;
 
-// Variables
-let isPresent = 1;
-let monthlyWage = 0;
-let empHrs = 0, empWage = 0;
-
-for(let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    switch(empCheck) {
+function getWorkingHours(empCheck){
+switch(empCheck) {
+    
     case IS_PART_TIME:
-        empHrs = 4;
-        break
+        return PART_TIME_HRS;
+
     case IS_FULL_TIME:
-        empHrs = 8;
-        break
+        return FULL_TIME_HRS;
     default:
-        empHrs = 0;
-    }
-    empWage = empHrs * EMP_RATE_PER_HOUR;
-    monthlyWage += empWage;
+        return 0;
+   }    
+}
+let empHrs=0;
+for (let day=0; day<NUM_OF_WORKING_DAYS; day++) {
+let empCheck=Math.floor(Math.random()*10)%3;
+empHrs += getWorkingHours(empCheck);
 }
 
-console.log("Employee Monthly Wage = " + monthlyWage);
+let empWage=empHrs* EMP_WAGE_PER_HRS;
+console.log("Total Hrs: "+ empHrs +"Employee Wage is :" + empWage);
